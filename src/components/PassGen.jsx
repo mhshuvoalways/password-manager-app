f2357f7bf6869f5692f2919adaf91832e5c1318f
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2023-01-21 19:42:42 +0600

    add checker

--- src/components/PassGen.jsx
+++ src/components/PassGen.jsx
@@ -11,22 +11,27 @@ const PassGen = () => {
       <div className="flex items-center gap-3 flex-wrap sm:flex-nowrap">
         <img src={Password} className="w-20" />
         <div className="space-y-3">
-          <p className={`text-3xl leading-normal sm:${styles.heading2}`}>Password Generator</p>
+          <p className={`text-3xl leading-normal sm:${styles.heading2}`}>
+            Password Generator
+          </p>
           <p>Generate randomized, strong, secure passwords</p>
         </div>
       </div>
-      <div className="flex items-center mt-10 gap-10 justify-between flex-wrap sm:flex-nowrap">
-        <div className="flex items-center w-full">
-          <input
-            type="email"
-            name="email"
-            className="bg-gray-700 appearance-none outline-0 px-3 h-16  rounded-l-lg text-white font-thin w-full"
-            value="&GQvlCw2;>HyX?{M"
-          />
-          <img
-            src={Copy}
-            className="w-10 text-white cursor-pointer bg-gray-700 rounded-r-lg h-16 pr-3"
-          />
+      <div className="flex mt-10 gap-10 justify-between items-center flex-wrap sm:flex-nowrap">
+        <div className="w-full">
+          <div className="flex items-center border-4 border-green-600">
+            <input
+              type="text"
+              name="text"
+              className="bg-gray-700 appearance-none outline-0 px-3 h-16 text-white font-thin w-full"
+              value="&GQvlCw2;>HyX?{M"
+            />
+            <img
+              src={Copy}
+              className="w-10 text-white cursor-pointer bg-gray-700 h-16 pr-3"
+            />
+          </div>
+          <p className="bg-green-600 py-2 text-center font-bold">Strong</p>
         </div>
         <button
           className="px-6 h-16 font-poppins text-primary bg-blue-gradient rounded-[10px] w-full sm:w-4/12"
@@ -52,28 +57,21 @@ const PassGen = () => {
                 type="checkbox"
                 className="w-5 h-5 rounded-lg cursor-pointer"
               />
-              <p className="cursor-pointer">Lowercase Letters</p>
+              <p className="cursor-pointer">Uppercase Letters</p>
             </label>
             <label className="flex items-center gap-3 flex-wrap">
               <input
                 type="checkbox"
                 className="w-5 h-5 rounded-lg cursor-pointer"
               />
-              <p className="cursor-pointer">Lowercase Letters</p>
+              <p className="cursor-pointer">Numbers</p>
             </label>
             <label className="flex items-center gap-3 flex-wrap">
               <input
                 type="checkbox"
                 className="w-5 h-5 rounded-lg cursor-pointer"
               />
-              <p className="cursor-pointer">Lowercase Letters</p>
-            </label>
-            <label className="flex items-center gap-3 flex-wrap">
-              <input
-                type="checkbox"
-                className="w-5 h-5 rounded-lg cursor-pointer"
-              />
-              <p className="cursor-pointer">Lowercase Letters</p>
+              <p className="cursor-pointer">Symbols</p>
             </label>
           </div>
         </div>
