6e84293722ab31bffab9985508d9907580e51a2b
MH Shuvo <mhshuvoalways@gmail.com>
2024-09-10 20:15:05 +0600

    refactor: transfer the backend from render to vercel

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,15 +1,8 @@
-import React, { useEffect } from "react";
-import Axios from "../utils/axios";
+import React from "react";
 import Context from "./Context";
 import Router from "./Router";
 
 const App = () => {
-  useEffect(() => {
-    Axios.get("/")
-      .then(() => {})
-      .catch(() => {});
-  }, []);
-
   return (
     <Context>
       <Router />
